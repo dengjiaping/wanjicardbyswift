24aa9c4d0a3b82a94bd3d894f85029a1ce8db05d
ShineMy <641494970@qq.com>
2017-11-08 13:26:06 +0800

    sd

---
+++ wanjicardbyswift/WanJiCard WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  WanJiCard WatchKit Extension
+//
+//  Created by Angie on 16/2/29.
+//  Copyright © 2016年 WJK. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ wanjicardbyswift/WanJiCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WanJiCard
+//
+//  Created by Angie on 16/2/29.
+//  Copyright © 2016年 WJK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
